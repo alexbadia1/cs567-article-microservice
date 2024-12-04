f8cb230cc961f958b2ce30c2b1d8b7622fc0b4a4
abadia2 <abadia2@illinois.edu>
2024-12-04 10:21:48 -0600

    fixed authtoken verification (consider moving token to bearer field)

--- middleware/auth/index.js
+++ middleware/auth/index.js
@@ -1,12 +1,15 @@
-const { admin } = require('../../config/firebase');
+const cookie = require('cookie');
+const { auth } = require('../../config/firebase');
 
 const authenticate = async (req, res, next) => {
   try {
-    const idToken = req.cookies.access_token;
-    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    const parsedCookies = cookie.parse(req.header('cookie'));
+    const idToken = parsedCookies.access_token;
+    const decodedToken = await auth.verifyIdToken(idToken);
     req.user = decodedToken;
     next();
   } catch (error) {
+    console.error('Error while verifying Firebase ID token:', error);
     return res.status(403).json({
       message: 'Unauthorized',
     });
