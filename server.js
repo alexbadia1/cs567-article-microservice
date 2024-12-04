1d8cd20f97ca3098e5ae594fb8c7ee061153d9f0
abadia2 <abadia2@illinois.edu>
2024-12-04 07:47:50 -0600

    allow credentials

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ mongoose.connect(secrets.mongo_connection, { useNewUrlParser: true });
 
 var allowCrossDomain = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
   res.header(
     'Access-Control-Allow-Headers',
     'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
